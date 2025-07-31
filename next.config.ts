367058a831d4bd4e8e48d511b91519001232a842
Arpan Padhi <arpanpadhi2004@gmail.com>
2025-07-31 22:10:13 +0530

    RG05

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
   // ***** THIS IS THE CRITICAL FIX *****
   // This line tells Next.js to build a dynamic, server-based application
   // which is required for platforms like Cloudflare and Netlify to run it correctly.
-  output: 'standalone',
+  
   
   /* other config options can go here */
 };
